c9530c96fc0c5495d262e587756d95e10698a37d
shinyfuwante <mr.philliptang@gmail.com>
2022-10-20 22:24:20 -0700

    Implement static layout for input form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import General from "../General";
+import EduInfo from "../EduInfo";
+import WorkInfo from "../WorkInfo";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h3 id="general-header">General Information</h3>
+      <General></General>
+      <h3 id="education-header">Education</h3>
+      <EduInfo></EduInfo>
+      <h3 id="work-header">Work History</h3>
+      <WorkInfo></WorkInfo>
+    </>
+  );
 }
 
 export default App;
