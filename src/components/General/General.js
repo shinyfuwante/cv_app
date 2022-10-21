f3741341472d6129b269c0f918555a696991a8bc
shinyfuwante <mr.philliptang@gmail.com>
2022-10-21 14:29:50 -0700

    Changing the formatting of General.js

--- src/components/General/General.js
+++ src/components/General/General.js
@@ -1,21 +1,40 @@
 import React from "react";
-import './General.css'
+import "./General.css";
 
 class General extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const {generalChange} = this.props;
-        return (
-        <div className = 'general-info'>
-        <form className = 'general-info-form'>
-            <input type="text" name="userName" id="userName" placeholder = "Full Name" onChange = {generalChange}/>
-            <input type="email" name="email" id="email" placeholder = "your@email.com" onChange = {generalChange}/>
-            <input type="tel" name="phoneNum" id="phoneNum" placeholder = "(111) 222-3333" onChange = {generalChange}/>
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { generalChange } = this.props;
+    return (
+      <div className="general-info">
+        <form className="general-info-form">
+          <input
+            type="text"
+            name="userName"
+            id="userName"
+            placeholder="Full Name"
+            onChange={generalChange}
+          />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="your@email.com"
+            onChange={generalChange}
+          />
+          <input
+            type="tel"
+            name="phoneNum"
+            id="phoneNum"
+            placeholder="(111) 222-3333"
+            onChange={generalChange}
+          />
         </form>
-        </div>)
-    }
+      </div>
+    );
+  }
 }
 
 export default General;
