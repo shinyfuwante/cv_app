6e12af5b171a4d5dd55ac227eda5dd0a6b2cc1c2
shinyfuwante <mr.philliptang@gmail.com>
2022-10-20 23:35:08 -0700

    Save input from general information component

--- src/components/General/General.js
+++ src/components/General/General.js
@@ -2,13 +2,17 @@ import React from "react";
 import './General.css'
 
 class General extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        const {generalChange} = this.props;
         return (
         <div className = 'general-info'>
         <form className = 'general-info-form'>
-            <input type="text" name="userName" id="userName" placeholder = "Full Name"/>
-            <input type="email" name="email" id="email" placeholder = "your@email.com"/>
-            <input type="tel" name="phoneNum" id="phoneNum" placeholder = "(111) 222-3333"/>
+            <input type="text" name="userName" id="userName" placeholder = "Full Name" onChange = {generalChange}/>
+            <input type="email" name="email" id="email" placeholder = "your@email.com" onChange = {generalChange}/>
+            <input type="tel" name="phoneNum" id="phoneNum" placeholder = "(111) 222-3333" onChange = {generalChange}/>
         </form>
         </div>)
     }
