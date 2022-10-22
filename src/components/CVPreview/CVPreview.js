aca43318d7de66cae8600d6314e57a8cfaf43d76
shinyfuwante <mr.philliptang@gmail.com>
2022-10-22 14:00:51 -0700

    Implement use of job/degree components in CVPreview

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Degree from "../Degree";
+import Job from "../Job";
 
 export default class CVPreview extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    const { name, email, phoneNum, education, workExperience } = this.props;
+    const { name, email, phoneNum, education, workExperience, deleteEdu } = this.props;
     return (
       <>
         <h3>{name}</h3>
@@ -14,31 +16,13 @@ export default class CVPreview extends Component {
         <h3>Education</h3>
         {education.map((degree) => {
           return (
-            <div
-              key={`${degree.uni} and ${degree.subject} and ${degree.degree}`}
-            >
-              <h4>{`${degree.degree} in ${degree.subject}`}</h4>
-              <h5>{degree.uni}</h5>
-              <h6>{degree.city}</h6>
-              <div className="eduDates">
-                {`${degree.startDate} to
-                            ${degree.endDate}`}
-              </div>
-            </div>
+            <Degree degree={degree} key={`${degree.uni} and ${degree.subject} and ${degree.degree}`} deleteEdu = {deleteEdu}/>
           );
         })}
         <h3>Work Experience</h3>
         {workExperience.map((job) => {
           return (
-            <div key={`${job.company} and ${job.title}`}>
-              <h4>{job.company}</h4>
-              <h5>{job.title}</h5>
-              <div className="workDates">
-                {`${job.startDate} to
-                            ${job.endDate}`}
-              </div>
-              {job.description}
-            </div>
+            <Job job = {job} key={`${job.company} and ${job.title}`}></Job>
           );
         })}
       </>
