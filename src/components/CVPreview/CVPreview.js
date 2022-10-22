c8ce863aa95193f098dcb7c782b39f9f439561df
shinyfuwante <mr.philliptang@gmail.com>
2022-10-22 00:34:04 -0700

    Put a "to" between start and end dates

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -5,7 +5,7 @@ export default class CVPreview extends Component {
     super(props);
   }
   render() {
-    const {name, email, phoneNum, education, workExperience} = this.props;
+    const { name, email, phoneNum, education, workExperience } = this.props;
     return (
       <>
         <h3>{name}</h3>
@@ -13,33 +13,34 @@ export default class CVPreview extends Component {
         <h6>{phoneNum}</h6>
         <h3>Education</h3>
         {education.map((degree) => {
-            return (
-                <div key = {`${degree.uni} and ${degree.subject} and ${degree.degree}`}>
-                    <h4>{`${degree.degree} in ${degree.subject}`}</h4>
-                    <h5>{degree.uni}</h5>
-                    <h6>{degree.city}</h6>
-                    <div className="eduDates">
-                        {degree.startDate}
-                        {degree.endDate}
-                    </div>
-                </div>
-        )})}
+          return (
+            <div
+              key={`${degree.uni} and ${degree.subject} and ${degree.degree}`}
+            >
+              <h4>{`${degree.degree} in ${degree.subject}`}</h4>
+              <h5>{degree.uni}</h5>
+              <h6>{degree.city}</h6>
+              <div className="eduDates">
+                {`${degree.startDate} to
+                            ${degree.endDate}`}
+              </div>
+            </div>
+          );
+        })}
         <h3>Work Experience</h3>
-        {
-            workExperience.map((job) => {
-                return (
-                    <div key = {`${job.company} and ${job.title}`}>
-                        <h4>{job.company}</h4>
-                        <h5>{job.title}</h5>
-                        <div className="workDates">
-                            {job.startDate}
-                            {job.endDate}
-                        </div>
-                        {job.description}
-                    </div>
-                )
-            })
-        }
+        {workExperience.map((job) => {
+          return (
+            <div key={`${job.company} and ${job.title}`}>
+              <h4>{job.company}</h4>
+              <h5>{job.title}</h5>
+              <div className="workDates">
+                {`${job.startDate} to
+                            ${job.endDate}`}
+              </div>
+              {job.description}
+            </div>
+          );
+        })}
       </>
     );
   }
