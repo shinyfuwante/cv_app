a1238a10cee9f1aae95a596644245a99fb97d591
shinyfuwante <mr.philliptang@gmail.com>
2022-10-21 22:59:21 -0700

    Implement education/work in preview

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -10,6 +10,36 @@ export default class CVPreview extends Component {
       <>
         <h3>{name}</h3>
         <h5>{email}</h5>
+        <h6>{phoneNum}</h6>
+        <h3>Education</h3>
+        {education.map((degree) => {
+            return (
+                <div key = {`${degree.uni} and ${degree.subject} and ${degree.degree}`}>
+                    <h4>{`${degree.degree} in ${degree.subject}`}</h4>
+                    <h5>{degree.uni}</h5>
+                    <h6>{degree.city}</h6>
+                    <div className="eduDates">
+                        {degree.startDate}
+                        {degree.endDate}
+                    </div>
+                </div>
+        )})}
+        <h3>Work Experience</h3>
+        {
+            workExperience.map((job) => {
+                return (
+                    <div key = {`${job.company} and ${job.title}`}>
+                        <h4>{job.company}</h4>
+                        <h5>{job.title}</h5>
+                        <div className="workDates">
+                            {job.startDate}
+                            {job.endDate}
+                        </div>
+                        {job.description}
+                    </div>
+                )
+            })
+        }
       </>
     );
   }
